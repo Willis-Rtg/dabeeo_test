d52323f3039b68c60bd172ea9b1e43d380e04844
= <kshwan1023@gmail.com>
2021-10-28 11:36:52 +0900

    setMapPosition have some error for position's pixel.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import marker from "./assets/marker.png";
 import styled from "styled-components";
 import { useEffect, useRef, useState } from "react";
 
+const MAP_WINDOW_WIDTH = 1024;
+const MAP_WINDOW_HEIGHT = 768;
+
 const SApp = styled.div`
   width: 100%;
   height: 100vh;
@@ -26,15 +29,18 @@ const WReset = styled.div`
     transform: scale(1);
   }
 `;
-const Map = styled.div`
-  width: 1024px;
-  height: 768px;
+const MapWindow = styled.div`
+  width: ${MAP_WINDOW_WIDTH}px;
+  height: ${MAP_WINDOW_HEIGHT}px;
   overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 `;
-const MapImg = styled.img``;
+const MapImg = styled.img`
+  position: absolute;
+`;
 const Marker = styled.img`
   position: absolute;
   top: 20px;
@@ -46,22 +52,62 @@ function App() {
   const [mark, setMark] = useState([]);
   const mapRef = useRef();
 
+  const [mapPosition, setMapPosition] = useState();
+
+  useEffect(() => {
+    setMapPosition({
+      x: mapRef.current.clientWidth / 2 - MAP_WINDOW_WIDTH / 2,
+      y: mapRef.current.clientHeight / 2 - MAP_WINDOW_HEIGHT / 2,
+    });
+  }, []);
+  useEffect(() => {
+    mapRef.current.style.left = `-${mapPosition?.x}px`;
+    mapRef.current.style.top = `-${mapPosition?.y}px`;
+  }, [mapPosition]);
+
+  // setPotition에 오류가 있음.
   const onMoveMap = (e) => {
-    console.log(e);
     const mapClientX = e.clientX;
+    console.log("🚀 ~ file: App.js ~ line 72 ~ onMoveMap ~ e", e);
+    console.log("mapClientX :", mapClientX);
+    console.log("mapPosition :", mapPosition);
     const mapClientY = e.clientY;
+    const offsetLeft = e.screenX;
+    const offsetTop = e.screenY;
+    if (mapClientX !== 0 || mapClientY !== 0) {
+      setMapPosition((prev) => {
+        return {
+          x: mapClientX / 2 - MAP_WINDOW_WIDTH / 2 + offsetLeft,
+          y: mapClientY / 2 - MAP_WINDOW_HEIGHT / 2 + offsetTop,
+        };
+      });
+    }
+    // window.addEventListener("mousemove", setPosition);
   };
 
+  // const setPosition = (e) => {
+  //   console.log("🚀 ~ file: App.js ~ line 88 ~ window.addEventListener ~ e", e);
+  //   setMapPosition((prev) => {
+  //     return { x: prev.x - e.x, y: prev.y + e.y };
+  //   });
+  // };
+  // const onMoveEndMap = () => {
+  //   console.log("end Drag");
+  // };
+
+  const windowRef = useRef();
   const onContextMenu = (e) => {
     blockContextMenu(e);
     const clientX = e.clientX;
     const clientY = e.clientY;
 
-    const mapx = mapRef.current.getBoundingClientRect().x;
-    const mapY = mapRef.current.getBoundingClientRect().y;
+    const windowX = windowRef.current.getBoundingClientRect().x;
+    const windowY = windowRef.current.getBoundingClientRect().y;
+    // const windowX = e.target.offsetLeft;
+    // const windowY = e.target.offsetTop;
     setMark((prev) => [
       ...prev,
-      { top: clientY - mapY - 50, left: clientX - mapx - 30 },
+      { top: clientY - windowY - 50, left: clientX - windowX - 30 },
     ]);
   };
 
@@ -70,22 +116,28 @@ function App() {
   return (
     <SApp>
       <WMap>
-        <Map ref={mapRef}>
+        <MapWindow ref={windowRef}>
           <MapImg
+            ref={mapRef}
+            // style={{
+            //   top: `${mapPosition?.y}px`,
+            //   left: `${mapPosition?.x}px`,
+            // }}
             src={map}
             alt="map"
             onDrag={onMoveMap}
+            // onDragEnd={onMoveEndMap}
             onContextMenu={onContextMenu}
           />
-        </Map>
+        </MapWindow>
         <WReset onClick={() => setMark([])}>
           <img src={reset} alt="reset" />
         </WReset>
-        {mark?.map((make) => (
+        {mark?.map((mark) => (
           <Marker
             src={marker}
             onContextMenu={blockContextMenu}
-            style={{ top: `${make?.top}px`, left: `${make?.left}px` }}
+            style={{ top: `${mark?.top}px`, left: `${mark?.left}px` }}
           />
         ))}
       </WMap>
